34dee92dccb28c1d46988622b785168c7906d163
Homiry <1733865392@qq.com>
2019-12-27 19:26:02 +0800

    第一次：初步实现blog的一些组件架构、样式

---
+++ miniprogram/components/bottom-modal/bottom-modal.js
@@ -0,0 +1,26 @@
+// components/bottom-modal/bottom-modal.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+  // apply-shared 表示页面 wxss 样式将影响到自定义组件，但自定义组件 wxss 中指定的样式不会影响页面
+  options: {
+    styleIsolation: 'apply-shared'
+  },
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
