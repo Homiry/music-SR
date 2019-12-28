0ed2b9b606002efabf6967ae10e6eaab38d4e11b
Homiry <1733865392@qq.com>
2019-12-28 20:34:08 +0800

    page-blog第二次：获取信息权限

--- miniprogram/components/bottom-modal/bottom-modal.js
+++ miniprogram/components/bottom-modal/bottom-modal.js
@@ -4,11 +4,14 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    modalShow: Boolean
   },
-  // apply-shared 表示页面 wxss 样式将影响到自定义组件，但自定义组件 wxss 中指定的样式不会影响页面
+  
   options: {
-    styleIsolation: 'apply-shared'
+    // apply-shared 表示页面 wxss 样式将影响到自定义组件，但自定义组件 wxss 中指定的样式不会影响页面
+    styleIsolation: 'apply-shared',
+    //启用多个具名插槽
+    multipleSlots: true,
   },
   /**
    * 组件的初始数据
@@ -21,6 +24,10 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    onClose(){
+      this.setData({
+        modalShow: false,
+      })
+    }
   }
 })
--- miniprogram/pages/blog/blog.js
+++ miniprogram/pages/blog/blog.js
@@ -5,12 +5,46 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    modalShow: false,   //控制底部弹出层是否显示
   },
 
   //发布功能
   onPublish(){
-    
+    //判断用户是否授权
+    wx.getSetting({
+      success: (res) => {
+        console.log(res)
+        if (res.authSetting['scope.userInfo']){
+          wx.getUserInfo({
+            success:(res)=>{
+              // console.log(res)
+            this.onLoginSuccess({
+              detail: res.userInfo
+            })
+            }
+          })  
+        }else{
+          this.setData({
+            modalShow: true,
+          })
+        }
+      }
+    })
+  },
+
+  onLoginSuccess(event){
+    console.log(event)
+    const detail = event.detail
+    wx.navigateTo({
+      url: `../blog-edit/blog-edit?nickName=${detail.nickName}&avatarUrl=${detail.avatarUrl}`,
+    })
+  },
+
+  onLoginFail(){
+    wx.showModal({
+      title: '授权用户才能发布',
+      content: '',
+    })
   },
 
   /**
